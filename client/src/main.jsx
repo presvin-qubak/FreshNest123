f1af1d8f0f5e16e788c36b6a68f4ef451be9e1fa
Reshaniresh <reshanireshibap@gmail.com>
2026-07-26 17:33:29 +0530

    base path fixes

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -9,21 +9,17 @@ import { DarkModeProvider } from "./context/DarkModeContext";
 import { NotificationProvider } from "./context/NotificationContext";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <BrowserRouter basename="/FreshNest123">
+    <LanguageProvider>
+      <DarkModeProvider>
+        <NotificationProvider>
+          <StoreContextProvider>
+            <App />
 
-  <LanguageProvider>
-    <DarkModeProvider>
-      <NotificationProvider>
-    <StoreContextProvider>
-      <App />
-
-      <Toaster
-        position="top-right"
-        reverseOrder={false}
-      />
-    </StoreContextProvider>
-    </NotificationProvider>
-    </DarkModeProvider>
+            <Toaster position="top-right" reverseOrder={false} />
+          </StoreContextProvider>
+        </NotificationProvider>
+      </DarkModeProvider>
     </LanguageProvider>
   </BrowserRouter>
 );
